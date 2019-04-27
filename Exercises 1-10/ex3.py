1444e2d87cfce8fb27b5d8e85f16e1bb079a3562
VageX <wfgcarty@gmail.com>
2019-04-27 18:14:21 -0500

    Added ex3.py

---
+++ Exercises 1-10/ex3.py
@@ -0,0 +1,25 @@
+# Simple statement what the succeeding calculations will do.
+print("I will now count my chickens:")
+# Calculating Hens and Roosters using order of operations
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+# Calculating eggs now
+print("Now I will count the eggs:")
+# Calculating eggs using order of operations
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+# Determine is first equation is less than second equation
+print(3 + 2 < 5 - 7)
+# Breaking down both equations in parts. First solve the first equation.
+print("What is 3 + 2?", 3 + 2)
+# Solve second equation.
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
